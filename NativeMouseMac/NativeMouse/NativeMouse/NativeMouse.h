5bbd8b0cdbbfa28bebb6f6ba13a07fa20a112019
Max Rozdobudko <max.rozdobudko@gmail.com>
2012-12-18 23:32:14 +0200

    Adds implementation for Mac

---
+++ NativeMouseMac/NativeMouse/NativeMouse/NativeMouse.h
@@ -0,0 +1,13 @@
+//
+//  NativeMouse2.h
+//  NativeMouse
+//
+//  Created by Max Rozdobudko on 17.12.12.
+//  Copyright (c) 2012 Max Rozdobudko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NativeMouse2 : NSObject
+
+@end
---
+++ NativeMouseMac/NativeMouse/NativeMouse/NativeMouse.m
@@ -0,0 +1,13 @@
+//
+//  NativeMouse2.m
+//  NativeMouse
+//
+//  Created by Max Rozdobudko on 17.12.12.
+//  Copyright (c) 2012 Max Rozdobudko. All rights reserved.
+//
+
+#import "NativeMouse2.h"
+
+@implementation NativeMouse2
+
+@end
